18eb96fe6a76e2ee37c962abb79fd58b0358a929
John Hunter <john@pushagency.io>
2017-06-14 13:00:22 -0400

    initial commit

---
+++ src/main/java/ConsumerLoop.java
@@ -0,0 +1,55 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by John on 6/14/17.
+ */
+public class ConsumerLoop implements Runnable {
+
+    private final KafkaConsumer<String, String> consumer;
+    private final List<String> topics;
+    private final int id;
+
+    public ConsumerLoop(int id,
+                        String groupId,
+                        List<String> topics) {
+        this.id = id;
+        this.topics = topics;
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", groupId);
+        props.put("key.deserializer", StringDeserializer.class.getName());
+        props.put("value.deserializer", StringDeserializer.class.getName());
+        this.consumer = new KafkaConsumer<>(props);
+    }
+
+    @Override
+    public void run() {
+        try {
+            consumer.subscribe(topics);
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Long.MAX_VALUE);
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println(this.id + ": " + record.value());
+                }
+            }
+        } catch (WakeupException e) {
+            // ignore for shutdown
+        } finally {
+            consumer.close();
+        }
+    }
+
+    public void shutdown() {
+        consumer.wakeup();
+    }
+}
---
+++ src/main/java/NotificationConsumerApp.java
@@ -0,0 +1,46 @@
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by John on 6/14/17.
+ */
+public class NotificationConsumerApp {
+    public static void main(String[] args) {
+        int numConsumers = 1;
+        String groupId = "notifications";
+        List<String> topics = Arrays.asList("notifications");
+        ExecutorService executor = Executors.newFixedThreadPool(numConsumers);
+
+        final List<ConsumerLoop> consumers = new ArrayList<>();
+        for (int i = 0; i < numConsumers; i++) {
+            ConsumerLoop consumer = new ConsumerLoop(i, groupId, topics);
+            consumers.add(consumer);
+            executor.submit(consumer);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                for (ConsumerLoop consumer : consumers) {
+                    consumer.shutdown();
+                }
+
+                executor.shutdown();
+                try {
+                    executor.awaitTermination(5000, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
